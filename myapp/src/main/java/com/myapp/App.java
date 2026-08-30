54561435af4c18edbf0a245e420546acea3004c1
sbouhnik <samuelbouhnik2@gmail.com>
2026-08-30 10:48:51 +0300

    Change greeting message in App.java

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Shmuel Bouhnik is saying you Hello World!" );
     }
 }
